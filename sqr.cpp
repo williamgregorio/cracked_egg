b2dfa0791cb708e9cbcabd7f75404f341cc5db29
William Gregorio <mrwilliamgregorio@yahoo.com>
2023-06-10 19:45:12 -0400

    hmm

--- sqr.cpp
+++ sqr.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 
-double square(double x){
+double square(double x)
+{
 	return x*x;
 }
+
+void print_square(double x)
+{
+	cout << "The square of " << x << " is " << square(x) << "\n";
+	
+}
+
+int main()
+{
+
+	print_square(1.234);
+
+}
+
---
+++ types.cpp
@@ -0,0 +1,32 @@
+//Fundamentals
+//bool
+//char
+//int
+//double
+
+
+
+
+//arith
+//x+y
+//+x  unary
+//x-y
+//-x
+//x*y
+//x/y
+//x%y
+//
+//^^ 
+//
+//
+
+
+void some_function()
+{
+	double d = 2.2;
+	int i = 7;
+	d = d+i //hmmm
+	i = d*i;
+}
+
+
