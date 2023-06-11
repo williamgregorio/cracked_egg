93d00bfc7d66920cfee096c269c77390ec6d2626
William Gregorio <mrwilliamgregorio@yahoo.com>
2023-06-10 20:29:25 -0400

    loops in

--- test_loop.cpp
+++ test_loop.cpp
@@ -35,3 +35,49 @@ bool accept2()
 	}
 }
 
+
+//or what if lets give user more chance
+
+
+
+
+bool accept3
+{
+
+	int tries = 1;
+	while(tries < 4){
+
+		cout << "Do you want to proceed (y or n)?\n";
+		char answer = 0;
+		cin >> answer;
+
+		switch(answer){
+		case 'y':
+			return true;
+		case 'n':
+			return false;
+		default:
+			cout << "Sorry, hmm I dont quite understand\n";
+			++tries //arthis for incre
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
