ce530c6253ff68fb235d62d53c231c2a91f76242
William Gregorio <mrwilliamgregorio@yahoo.com>
2023-06-10 21:01:57 -0400

    points

---
+++ points_arrs_loop.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+	return 0;
+}
+
+//arr of elements 
+//char v[6]; < 6 chars
+//
+//**pointer**
+//
+//char* p;
+//
+
+
+//char* p = &v[3] 4ths as 0 5
+//char x = *p
+//
+//
+//
+//for example
+//
+//p: [] -----> v [0,1,2,(*3*),4,5]
+//
+//
+//copy ten elements from one v to another
+//
+//
+//
+
+
+
+
+void copy_fct()
+{
+	int v1[10] = {0,1,2,3,4,5,6,7,8,9};
+	int v2[10]'
+
+	for (auto i=0; i!=10; i++)
+		v2[i]=v1[i]
+}
+
+
+void print()
+{
+
+	int v[] = {0,1,2,3,4,5,6,7,8,9};
+	for (auto x:v)
+		cout << x << '\n';
+
+	for (auto x: {10,21,32,43,54,65})
+		cout << x << '\n';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
